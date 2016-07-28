82756430730df54b668667ae8f956f61647ab38b
hands-rec <hands.records@gmail.com>
2016-07-28 20:45:02 +0900

    Fix SSL error

--- spec/views_net_spec.rb
+++ spec/views_net_spec.rb
@@ -4,4 +4,14 @@ describe ViewsNet do
   it 'has a version number' do
     expect(ViewsNet::VERSION).not_to be nil
   end
+
+  describe "ViewsNet::Client" do
+    it "#payment_amount" do
+      client = ViewsNet::Client.new('./test/test.yaml')
+      expect(client.payment_amount[:month]).to match 'お客さまのお支払総額'
+      expect(client.payment_amount[:amount]).to match '円'
+    end
+  end
+
+
 end
